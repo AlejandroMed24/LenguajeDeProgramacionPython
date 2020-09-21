925de81994eaf9b58c58af4cd86eb0aa29628dad
Antonio <antonio.alejandro.medina.moreno@uabc.edu.mx>
2020-09-21 14:15:37 -0700

    21/09/2020

--- test.py
+++ test.py
@@ -1,1 +1,41 @@
 print("hola mundo")
+
+mi_variable = 15
+#MiVariable = 15
+#mivariable=15
+print(mi_variable)
+
+#Comilla sencilla se utiliza cuando sea solo un caracter
+mi_cadena = 'Hola Mundo!'
+print(mi_cadena)
+
+mi_cadena1 = "Hola Mundo!"
+print(mi_cadena1)
+
+#println no existe en python
+
+#Numero entero
+mi_edad = 35
+print(mi_edad)
+
+#Numero entero octal
+mi_edad1 = 0o43
+print(oct(mi_edad1))
+
+#Numero entero hexadecimal
+mi_edad2 = 0x23
+print(hex(mi_edad2))
+
+#Numero real
+precio = 7435.28
+print(precio)
+
+#Booleano(verdadero/falso)
+verdadero = True
+falso = False
+
+#Cadena larga """
+mi_cadena_multilinea = """
+Esta es una cadena
+de varias lineas
+"""
